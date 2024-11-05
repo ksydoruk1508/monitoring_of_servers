d5b65f8117644309571d4e266b67dab43a61f090
Andante <84418705+ksydoruk1508@users.noreply.github.com>
2024-11-06 01:13:52 +0200

    Update Monitoring_of_servers.sh

--- Monitoring_of_servers.sh
+++ Monitoring_of_servers.sh
@@ -123,7 +123,7 @@ function add_server_to_monitoring {
     echo -e "${YELLOW}Введите IP-адрес нового сервера для добавления:${NC}"
     read new_ip
 
-    sudo sed -i "/job_name: 'node_exporter'/!b;n;c\      - targets: ['localhost:9090', '$new_ip:9100']" "$CONFIG_FILE"
+    sudo sed -i "/job_name: 'node_exporter'/,/targets: \[/ s/\(targets: \[.*\)\]/\1, '$new_ip:9100']/" "$CONFIG_FILE"
     echo -e "${GREEN}Сервер $new_ip успешно добавлен в список наблюдения!${NC}"
 
     echo -e "${BLUE}Перезапускаем Prometheus...${NC}"
